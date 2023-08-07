70219859b5afb9a456258542d79b9c740a495e57
Yassin Mehlil <yassin.mehlil@gmail.com>
2023-08-07 12:26:51 +0200

    add Cat class

---
+++ ex00/Cat.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Cat.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ymehlil <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/07 11:54:01 by ymehlil           #+#    #+#             */
+/*   Updated: 2023/08/07 12:01:27 by ymehlil          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Cat.hpp"
+
+Cat::Cat() : Animal("Cat") {
+    std::cout << BLUE << "Cat default constructor called" << RESET << std::endl;
+}
+
+Cat::Cat(const Cat& src) : Animal(src) {
+    std::cout << BLUE << "Cat copy constructor called" << RESET << std::endl;
+    *this = src;
+}
+
+Cat& Cat::operator=(const Cat& rhs) {
+    if (this != &rhs)
+        this->type = rhs.getType();
+    return *this;
+}
+
+void Cat::makeSound() const {
+    std::cout << BLUE << "* miaou *" << RESET << std::endl;
+}
+
+Cat::~Cat() {
+    std::cout << BLUE << "Cat destructor called" << RESET << std::endl;
+}
--- ex00/Cat.hpp
+++ ex00/Cat.hpp
@@ -1,27 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Cat.hpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ymehlil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/06 19:37:08 by ymehlil           #+#    #+#             */
-/*   Updated: 2023/08/07 12:18:37 by ymehlil          ###   ########.fr       */
+/*   Created: 2023/08/07 11:51:41 by ymehlil           #+#    #+#             */
+/*   Updated: 2023/08/07 12:13:42 by ymehlil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Animal.hpp"
-#include "Dog.hpp"
-#include "Cat.hpp"
+#ifndef CAT_HPP
+# define CAT_HPP
+# include "Animal.hpp"
 
-int main() {
-	Animal test;
-	Dog chien;
-	Cat chat;
+class Cat : public Animal {
+    public:
+        Cat();
+        Cat(const Cat& src);
+        ~Cat();
+        Cat& operator=(const Cat &rhs);
 
-	test.makeSound();
-	chien.makeSound();
-	chat.makeSound();
-	
-}
+        void makeSound() const;
+};
 
+#endif
